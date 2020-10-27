c83bbcd69ca0a269accfb674997e0228b01ba51f
Crowdbotics <team@crowdbotics.com>
2020-10-27 02:03:46 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/webond_21993/MainActivity.java
@@ -0,0 +1,15 @@
+package com.webond_21993;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "webond_21993";
+    }
+}
